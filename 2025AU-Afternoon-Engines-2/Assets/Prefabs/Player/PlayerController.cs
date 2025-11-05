2d0d769ffb1a63a38c8b711df53152a18fc99a4c
NeonFinn <n3onfinn@gmail.com>
2025-11-04 23:24:58 -0500

    Revert cursor visible to false

--- 2025AU-Afternoon-Engines-2/Assets/Prefabs/Player/PlayerController.cs
+++ 2025AU-Afternoon-Engines-2/Assets/Prefabs/Player/PlayerController.cs
@@ -46,12 +46,8 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
 
-        //add by thomas for testing the Score/Score system system need the Cursor
         Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
- 
-
-
+        Cursor.visible = false;
     }
 
     private void Update()
